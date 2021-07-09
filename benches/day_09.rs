44e6cfb88fef09a9444af6f57c5e7d83fe520f6e
Kenzi Jeanis <isavegas2006@gmail.com>
2021-07-09 01:39:14 -0500

    Format benchmarks

--- benches/day_09.rs
+++ benches/day_09.rs
@@ -1,5 +1,5 @@
-use lazy_static::lazy_static;
 use intcode::{parse_intcode, IntCodeMachine, Num};
+use lazy_static::lazy_static;
 
 // We don't want to include time to parse intcode in the benchmarks.
 lazy_static! {
@@ -9,10 +9,14 @@ lazy_static! {
 
 #[inline(never)]
 pub fn part_1(f: usize) {
-    IntCodeMachine::new(INTCODE.clone(), vec![1], f).execute().expect("Error running machine!");
+    IntCodeMachine::new(INTCODE.clone(), vec![1], f)
+        .execute()
+        .expect("Error running machine!");
 }
 
 #[inline(never)]
 pub fn part_2(f: usize) {
-    IntCodeMachine::new(INTCODE.clone(), vec![2], f).execute().expect("Error running machine!");
+    IntCodeMachine::new(INTCODE.clone(), vec![2], f)
+        .execute()
+        .expect("Error running machine!");
 }
